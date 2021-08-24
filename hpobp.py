31adbe536170445f481a397ce7ebeb48e105e6a4
hpo1988bp <89401130+hpo1988bp@users.noreply.github.com>
2021-08-24 11:55:51 +0700

    Update hpobp.py

--- hpobp.py
+++ hpobp.py
@@ -554,7 +554,7 @@ def FShare(path="0", tracking_string="FShare"):
 				urllib.quote_plus(path),
 				urllib.quote_plus(tracking_string)
 			),
-			'thumbnail': "https://docs.google.com/drawings/d/12OjbFr3Z5TCi1WREwTWECxNNwx0Kx-FTrCLOigrpqG4/pub?w=256&h=256"
+			'thumbnail': "https://i.postimg.cc/0Qqgpd1H/6909780.png"
 		})
 	plugin.add_sort_method(xbmcplugin.SORT_METHOD_UNSORTED)
 	plugin.add_sort_method(xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
